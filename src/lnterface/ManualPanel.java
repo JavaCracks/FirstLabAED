1a79f3514efeb320177c2c474d1d64c15b562e09
juandavidpaz <juandavidpaz@192.168.1.8>
2018-08-23 14:43:17 -0500

    interface error is solved

--- src/lnterface/ManualPanel.java
+++ src/lnterface/ManualPanel.java
@@ -1,5 +1,6 @@
 package lnterface;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.TextField;
@@ -12,42 +13,70 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
-public class ManualPanel extends JPanel implements ActionListener {
+public class ManualPanel extends JPanel implements ActionListener{
 
-	public final static String SORT = "SORT";
+	public final static String SORT= "SORT";
+	public final static String BACK = "BACK";
 
 	private JButton sort;
 	private JLabel manual;
 	private JLabel input;
 	private JTextField inputTxt;
+	private JButton buttonBack;
 
+	
 	public ManualPanel() {
-		setLayout(null);
-		TitledBorder border = new TitledBorder("Sorting Elements");
-		setBorder(border);
+	setLayout(null);
+	TitledBorder border = new TitledBorder("Sorting Elements");
+	setBorder(border);
+	
+	 buttonBack = new JButton(BACK);
+	 buttonBack.addActionListener(this);
+	 buttonBack.setActionCommand(BACK);
+	 buttonBack.setBounds(1, 330, 70, 30);
+	    sort = new JButton(SORT);
+	    sort.addActionListener(this);
+	    sort.setActionCommand(SORT);
+	    sort.setLayout(null);
+	    sort.setBounds(240, 250, 100, 50);
+	    
+	    manual= new JLabel("MANUAL");
+	    manual.setLayout(null);
+	    manual.setFont(new Font("ARIAL", 1, 20 ));
+	    manual.setBounds(250,100,100,50); 
+	    
+	    input= new JLabel("Input:");
+	    input.setLayout(null);
+	    input.setFont(new Font("ARIAL", 1, 16 ));
+	    input.setBounds(120,160,80,50); 
 
-		sort = new JButton();
-		sort.addActionListener(this);
-		sort.setActionCommand(SORT);
-		sort.setLayout(null);
-		sort.setBounds(210, 100, 100, 50);
+	    
+	    
+	    inputTxt = new JTextField();
+	    inputTxt.setLayout(null);
+	    inputTxt.setBounds(200,170,250,30); 
 
-		manual = new JLabel("MANUAL");
-		manual.setLayout(null);
-
-		input = new JLabel("Input:");
-		input.setLayout(null);
-
-		inputTxt = new JTextField();
-		inputTxt.setLayout(null);
-
-		add(sort);
+	    
+	    
+	    
+	     add(sort);
+	     add(manual);
+	     add(input);
+	     add(inputTxt);
+	     add(buttonBack);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
+	
 
 }
+
+
+
+
+
+
--- src/lnterface/RandomPanel.java
+++ src/lnterface/RandomPanel.java
@@ -1,5 +1,7 @@
 package lnterface;
 
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,11 +10,17 @@ import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
-public class RandomPanel extends JPanel implements ActionListener {
-
+public class RandomPanel extends JPanel  implements ActionListener{
+	
 	public final static String GENERATE = "GENERATE";
 	public final static String OUTPUT = "OUTPUT";
+	public final static String BACK = "BACK";
+
+	
+
+	
 
 	private JPanel panelSetup;
 	private JPanel panelOrder;
@@ -29,53 +37,166 @@ public class RandomPanel extends JPanel implements ActionListener {
 	private JLabel labelQuantity;
 	private JLabel labelSetupMsj;
 	private JLabel labelPercentage;
-	private JTextField quantityTxt;
-	private JTextField percentageTxt;
+	private JTextField  quantityTxt;
+	private JTextField  percentageTxt;
 	private JButton buttonGenerate;
 	private JButton buttonOutput;
+	private JButton buttonBack;
 
-	public RandomPanel() {
+	
 
-		// first panel
-		panelSetup = new JPanel();
-		panelSetup.setLayout(null);
-		checkInteger = new JCheckBox();
-		checkRational = new JCheckBox();
-		checkAll = new JCheckBox();
-		checkRepeated = new JCheckBox();
-		labelQuantity = new JLabel();
-		labelSetupMsj = new JLabel();
-		quantityTxt = new JTextField();
 
-		// second panel
+	
+ public RandomPanel () {
+	  
+	 
+	 
+	 setLayout(new GridLayout(4,1));
+	 TitledBorder border = new TitledBorder("Setup to generate");
+		
+	 buttonBack = new JButton(BACK);
+	 buttonBack.addActionListener(this);
+	 buttonBack.setActionCommand(BACK);
+	 buttonBack.setBounds(1, 50, 70, 30);
+	 //first panel
+	 panelSetup = new JPanel();
+	 panelSetup.setLayout(null);
+		panelSetup.setBorder(border);
 
-		panelOrder = new JPanel();
-		checkSort = new JCheckBox();
-		checkSortInversely = new JCheckBox();
-		checkRandom = new JCheckBox();
+	 panelSetup.setBounds(0,0,150,400);
+	 checkInteger = new JCheckBox("Integer");
+	 checkInteger.setBounds(20,20,100,20);
+	 checkInteger.setLayout(null);
+	 checkRational = new JCheckBox("Rational");
+	 checkRational.setBounds(20, 60, 100,20);
+	 checkRational.setLayout(null);
 
-		// third panel
+	 checkAll = new JCheckBox("All Diferents");
+	 checkAll.setLayout(null);
+	 checkAll.setBounds(120, 20, 150, 20);
 
-		panelGenerate = new JPanel();
-		checkDisorder = new JCheckBox();
-		labelPercentage = new JLabel();
-		percentageTxt = new JTextField();
-		buttonGenerate = new JButton();
-		buttonGenerate.addActionListener(this);
-		buttonGenerate.setActionCommand(GENERATE);
 
-		// fourth panel
+	 checkRepeated = new JCheckBox("Repeated Numbers");
+	 checkRepeated.setLayout(null);
+	 checkRepeated.setBounds(120, 60, 150, 20);
 
-		buttonOutput = new JButton();
-		buttonOutput.addActionListener(this);
-		buttonOutput.setActionCommand(OUTPUT);
+	 labelQuantity = new JLabel("Quantity:");
+	 labelQuantity.setFont(new Font("Arial", 1, 16));
+	 labelQuantity.setLayout(null);
+	 labelQuantity.setBounds(310,40,100,20);
+	 labelSetupMsj = new JLabel();
+	 labelSetupMsj.setLayout(null);
 
-	}
+	 quantityTxt = new JTextField();
+	 quantityTxt.setLayout(null);
+	 quantityTxt.setBounds(400,40,150,20);
 
+	 
+	 panelSetup.add(checkInteger);
+	 panelSetup.add(checkRational);
+	 panelSetup.add(checkAll);
+	 panelSetup.add(checkRepeated);
+	 panelSetup.add(labelQuantity);
+	 panelSetup.add(labelSetupMsj);
+	 panelSetup.add(quantityTxt);
+     
+	 
+
+
+	 
+	 
+	 //second panel
+	 
+	 panelOrder = new JPanel();
+	 panelOrder.setLayout(null);
+	 TitledBorder border1 = new TitledBorder(" ");
+      panelOrder.setBorder(border1);
+	 
+	 checkSort = new JCheckBox("Sorted");
+	 checkSort.setLayout(null);
+	 checkSort.setBounds(250, 50,160,40);
+	 checkSortInversely = new JCheckBox("Sorted Inversely");
+	 checkSortInversely.setLayout(null);
+	 checkSortInversely.setBounds(300, 15, 160, 40);
+	 checkRandom = new JCheckBox("Random Sorter");
+	 checkRandom.setLayout(null);
+	 checkRandom.setBounds(160,15,160,40);
+	 
+	 
+	 panelOrder.add(checkSort);
+	 panelOrder.add(checkSortInversely);
+	 panelOrder.add(checkRandom);
+
+
+	 
+	 
+
+	 //third panel
+	 
+	 panelGenerate = new JPanel();
+	 panelGenerate.setLayout(null);
+	 TitledBorder border2 = new TitledBorder(" ");
+     panelGenerate.setBorder(border2);
+	 checkDisorder = new JCheckBox("Disorder %");
+	 checkDisorder.setBounds(110,20,100,20);
+	 checkDisorder.setLayout(null);
+	 labelPercentage = new JLabel("Percentage:");
+	 labelPercentage.setBounds(110,60,100,20);
+	 labelPercentage.setLayout(null);
+	 percentageTxt = new JTextField();
+	 percentageTxt.setLayout(null);
+	 percentageTxt.setBounds(190, 60, 100, 20);
+	 buttonGenerate = new JButton(GENERATE);
+	 buttonGenerate.addActionListener(this);
+	 buttonGenerate.setActionCommand(GENERATE);
+	 buttonGenerate.setLayout(null);
+	 buttonGenerate.setBounds(350, 30, 100, 40);
+	 panelGenerate.add(checkDisorder);
+	 panelGenerate.add(labelPercentage);
+	 panelGenerate.add(percentageTxt);
+	 panelGenerate.add(buttonGenerate);
+	 
+	 
+	 
+	 //fourth panel
+	 
+	 panelOutput = new JPanel();
+	 panelOutput.setLayout(null);
+	 buttonOutput = new JButton(OUTPUT);
+	 buttonOutput.addActionListener(this);
+	 buttonOutput.setActionCommand(OUTPUT);
+	 buttonOutput.setLayout(null);
+	 buttonOutput.setBounds(240, 30, 100, 40);
+
+	 
+
+ panelOutput.add(buttonOutput);
+ panelOutput.add(buttonBack);
+
+ 
+	 
+	 
+
+	 
+     add(panelSetup);
+     add(panelOrder);
+     add(panelGenerate);
+     add(panelOutput);
+
+	 
+ }
+
+
+	
+
+
+	
+
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
