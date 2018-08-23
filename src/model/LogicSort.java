fcf134a33e63dd25a3d91f39d32291d04e4a485c
danie <danie@172.30.192.150>
2018-08-23 18:40:53 -0500

    Tim Sort method has been implemented

--- src/model/LogicSort.java
+++ src/model/LogicSort.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Arrays;
+
 public class LogicSort {
 
 	
@@ -64,5 +66,174 @@ public class LogicSort {
 		return arr;
 
 	}
+	
+	private void insertionSort(int array[], int left, int right) {
+	
+		for (int i=left+1;i<=right;i++) {
+			int temp=array[i];
+			int j=i-1;
+			
+			while(array[j]> temp && j>=left) {
+				array[j+1]=array[j];
+				j--;
+				
+			}
+			array[j+1]=temp;
+		}
+	}
+	
+	private void insertionSortDouble(double array[], int left, int right) {
+		
+		for (int i=left+1;i<=right;i++) {
+		    double temp=array[i];
+			int j=i-1;
+			
+			while(array[j]> temp && j>=left) {
+				array[j+1]=array[j];
+				j--;
+				
+			}
+			array[j+1]=temp;
+		}
+	}
+	
+	private void merge(int array[], int l, int m, int r) {
+	
+		int len1=m-l+1, len2=r-m;
+		int left[]=new int[len1];
+		int right[]=new int[len2];
+		
+		for(int i=0; i<len1;i++) {
+		
+			left[i]=array[l+i];			
+		}
+		
+		for(int i=0; i<len2;i++) {
+			right[i]=array[m+1+i];
+		}
+		
+		int i=0;
+		int j=0;
+		int k=0;
+		
+		while (i < len1 && j < len2) {
+	    
+	        if (left[i] <= right[j])
+	        {
+	            array[k] = left[i];
+	            i++;
+	        }
+	        else
+	        {
+	            array[k] = right[j];
+	            j++;
+	        }
+	        k++;
+	    }
+		
+		while (i < len1) {
+	    
+	        array[k] = left[i];
+	        k++;
+	        i++;
+	    }
+		
+		while (j < len2) {
+	    
+	        array[k] = right[j];
+	        k++;
+	        j++;
+	    }
+
+
+	
+		
+	}
+	
+	private void mergeDouble(double array[], int l, int m, int r) {
+		
+		int len1=m-l+1, len2=r-m;
+		double left[]=new double[len1];
+		double right[]=new double[len2];
+		
+		for(int i=0; i<len1;i++) {
+		
+			left[i]=array[l+i];			
+		}
+		
+		for(int i=0; i<len2;i++) {
+			right[i]=array[m+1+i];
+		}
+		
+		int i=0;
+		int j=0;
+		int k=0;
+		
+		while (i < len1 && j < len2) {
+	    
+	        if (left[i] <= right[j])
+	        {
+	            array[k] = left[i];
+	            i++;
+	        }
+	        else
+	        {
+	            array[k] = right[j];
+	            j++;
+	        }
+	        k++;
+	    }
+		
+		while (i < len1) {
+	    
+	        array[k] = left[i];
+	        k++;
+	        i++;
+	    }
+		
+		while (j < len2) {
+	    
+	        array[k] = right[j];
+	        k++;
+	        j++;
+	    }
+
+
+	
+		
+	}
+	
+	
+	public void timSort(int array[], int n) {
+		
+		for(int i=0; i<n; i+=32) {
+			insertionSort(array,i, Math.min((i+31),(n-1)));
+			
+		}
+		
+		for(int size=32; size<n;size=2*size) {
+			
+			for(int left=0; left<n; left+=2*size) {
+				int mid=left+size-1;
+				int right= Math.min((left+2*size-1),(n-1));
+				
+				merge(array,left,mid,right);
+			}
+		}
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int arreglo[]={12,23,15,4,7};
+		LogicSort l=new LogicSort();
+		l.timSort(arreglo,arreglo.length-1);
+		Arrays.toString(arreglo);
+		
+	}
+	
+	
 
 }
