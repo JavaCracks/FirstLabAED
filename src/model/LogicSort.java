c7d59a754afc19712fb85f9205ea2f0a4ea6c3f3
velez <velez@JuanchoVelezPro>
2018-08-23 19:42:07 -0500

    The method Tim Sort has been changed by Merge Sort.

--- src/model/LogicSort.java
+++ src/model/LogicSort.java
@@ -4,236 +4,111 @@ import java.util.Arrays;
 
 public class LogicSort {
 
-	
-	
 	public LogicSort() {
 
 	}
-	
 
 	public int[] countingSort(int[] arr) {
 
-			int n = arr.length-1;
-			int k = arr[0];
-			for (int i = 1; i <= n; i++) {
+		int n = arr.length - 1;
+		int k = arr[0];
+		for (int i = 1; i <= n; i++) {
 			if (arr[i] > k) {
-			k = arr[i];
+				k = arr[i];
 			}
-			}
-			int[] C = new int[k + 1];
+		}
+		int[] C = new int[k + 1];
 
-			for (int i = 0; i <= k; i++) {
+		for (int i = 0; i <= k; i++) {
 			C[i] = 0;
-			}
-			for (int i = 0; i <= n; i++) {
+		}
+		for (int i = 0; i <= n; i++) {
 			C[arr[i]] = C[arr[i]] + 1;
-			}
-			for (int i = 1; i <= k; i++) {
+		}
+		for (int i = 1; i <= k; i++) {
 			C[i] = C[i] + C[i - 1];
-			}
-			int[] B = new int[n+1];
-			for (int i = n ; i >= 0; i--) {
+		}
+		int[] B = new int[n + 1];
+		for (int i = n; i >= 0; i--) {
 			C[arr[i]] = C[arr[i]] - 1;
 			B[C[arr[i]]] = arr[i];
-			}
-			
-			return B;
-			
-
-	}
-
-	public int[] timSort(int[] arr) {
-          
-		return arr;
-
-	}
-
-	public double[] timSort(double[] arr) {
-
-		return arr;
-
-		
-	}
-
-	public int[] quickSort(int[] arr) {
-
-		return arr;
-
-	}
-
-	public double[] quickSort(double[] arr) {
-
-		return arr;
-
-	}
-	
-	private void insertionSort(int array[], int left, int right) {
-	
-		for (int i=left+1;i<=right;i++) {
-			int temp=array[i];
-			int j=i-1;
-			
-			while(array[j]> temp && j>=left) {
-				array[j+1]=array[j];
-				j--;
-				
-			}
-			array[j+1]=temp;
 		}
-	}
-	
-	private void insertionSortDouble(double array[], int left, int right) {
-		
-		for (int i=left+1;i<=right;i++) {
-		    double temp=array[i];
-			int j=i-1;
-			
-			while(array[j]> temp && j>=left) {
-				array[j+1]=array[j];
-				j--;
-				
-			}
-			array[j+1]=temp;
-		}
-	}
-	
-	private void merge(int array[], int l, int m, int r) {
-	
-		int len1=m-l+1, len2=r-m;
-		int left[]=new int[len1];
-		int right[]=new int[len2];
-		
-		for(int i=0; i<len1;i++) {
-		
-			left[i]=array[l+i];			
-		}
-		
-		for(int i=0; i<len2;i++) {
-			right[i]=array[m+1+i];
-		}
-		
-		int i=0;
-		int j=0;
-		int k=0;
-		
-		while (i < len1 && j < len2) {
-	    
-	        if (left[i] <= right[j])
-	        {
-	            array[k] = left[i];
-	            i++;
-	        }
-	        else
-	        {
-	            array[k] = right[j];
-	            j++;
-	        }
-	        k++;
-	    }
-		
-		while (i < len1) {
-	    
-	        array[k] = left[i];
-	        k++;
-	        i++;
-	    }
-		
-		while (j < len2) {
-	    
-	        array[k] = right[j];
-	        k++;
-	        j++;
-	    }
 
+		return B;
 
-	
-		
 	}
-	
-	private void mergeDouble(double array[], int l, int m, int r) {
-		
-		int len1=m-l+1, len2=r-m;
-		double left[]=new double[len1];
-		double right[]=new double[len2];
-		
-		for(int i=0; i<len1;i++) {
-		
-			left[i]=array[l+i];			
-		}
-		
-		for(int i=0; i<len2;i++) {
-			right[i]=array[m+1+i];
-		}
-		
-		int i=0;
-		int j=0;
-		int k=0;
-		
-		while (i < len1 && j < len2) {
-	    
-	        if (left[i] <= right[j])
-	        {
-	            array[k] = left[i];
-	            i++;
-	        }
-	        else
-	        {
-	            array[k] = right[j];
-	            j++;
-	        }
-	        k++;
-	    }
-		
-		while (i < len1) {
-	    
-	        array[k] = left[i];
-	        k++;
-	        i++;
-	    }
-		
-		while (j < len2) {
-	    
-	        array[k] = right[j];
-	        k++;
-	        j++;
-	    }
 
+	static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
 
-	
-		
-	}
-	
-	
-	public void timSort(int array[], int n) {
-		
-		for(int i=0; i<n; i+=32) {
-			insertionSort(array,i, Math.min((i+31),(n-1)));
-			
-		}
-		
-		for(int size=32; size<n;size=2*size) {
-			
-			for(int left=0; left<n; left+=2*size) {
-				int mid=left+size-1;
-				int right= Math.min((left+2*size-1),(n-1));
-				
-				merge(array,left,mid,right);
+		// index of smaller element
+		int i = (low - 1);
+		for (int j = low; j <= high - 1; j++) {
+			// If current element is smaller than or
+			// equal to pivot
+			if (arr[j] <= pivot) {
+				i++;
+
+				// swap arr[i] and arr[j]
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
 		}
-		
-		
-		
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+
+		return i + 1;
 	}
-	
+
+	public void quickSort(int arr[], int l, int h) {
+		// Create an auxiliary stack
+		int[] stack = new int[h - l + 1];
+
+		// initialize top of stack
+		int top = -1;
+
+		// push initial values of l and h to stack
+		stack[++top] = l;
+		stack[++top] = h;
+
+		// Keep popping from stack while is not empty
+		while (top >= 0) {
+			// Pop h and l
+			h = stack[top--];
+			l = stack[top--];
+
+			// Set pivot element at its correct position
+			// in sorted array
+			int p = partition(arr, l, h);
+
+			// If there are elements on left side of pivot,
+			// then push left side to stack
+			if (p - 1 > l) {
+				stack[++top] = l;
+				stack[++top] = p - 1;
+			}
+
+			// If there are elements on right side of pivot,
+			// then push right side to stack
+			if (p + 1 < h) {
+				stack[++top] = p + 1;
+				stack[++top] = h;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		
-		int arreglo[]={12,23,15,4,7};
-		LogicSort l=new LogicSort();
-		l.timSort(arreglo,arreglo.length-1);
-		Arrays.toString(arreglo);
-		
+
+		int arreglo[] = { 12, 23, 15, 4, 7, 8, 1, 2, 3 };
+
+		LogicSort l = new LogicSort();
+
+		System.out.println(Arrays.toString(l.countingSort(arreglo)));
+
 	}
-	
-	
 
 }
--- src/model/Random.java
+++ src/model/Random.java
@@ -12,6 +12,7 @@ public class Random {
 	
 	
 	
+	
 	public Random() {
 		
 		
@@ -19,6 +20,74 @@ public class Random {
 		
 	}
 	
+	
+	
+	public void setQuantity(int quantity) {
+		
+	this.quantity = quantity;	
+		
+	}
+	
+	public int getQuantity() {
+		
+	return quantity;		
+		
+	}
+	
+	
+	public void setRepeated(boolean repeated) {
+		
+	this.repeated = repeated;	
+		
+	}
+	
+	public boolean isRepeated() {
+		
+	return repeated;	
+		
+	}
+	
+	public void setInversely(boolean inversely) {
+		
+	this.inversely = inversely;
+		
+	}
+	
+	public boolean isInversely() {
+		
+	return inversely;	
+		
+	}
+	
+	public void setRandomOrder(boolean randomOrder) {
+		
+	this.randomOrder = randomOrder;	
+		
+	}
+	
+	public boolean isRandomOrder() {
+		
+	return randomOrder;	
+		
+	}
+	
+	public void setPercentageDisorder(double percentageDisorder) {
+		
+		this.percentageDisorder = percentageDisorder;
+		
+	}
+	
+	public double getPercentageDisorder() {
+		
+	return percentageDisorder;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	System.out.println();	
+		
+	}
 	
 	
 	
