2be4e99f43751c3518e20543461bdf605b6d9d13
danie <danie@172.30.192.150>
2018-08-23 20:06:10 -0500

    Add the methods with double value

--- src/model/LogicSort.java
+++ src/model/LogicSort.java
@@ -98,6 +98,66 @@ public class LogicSort {
 		}
 	}
 	
+	static int partitionDouble(double arr[], int low, int high) {
+		double pivot = arr[high];
+
+		// index of smaller element
+		int i = (low - 1);
+		for (int j = low; j <= high - 1; j++) {
+			// If current element is smaller than or
+			// equal to pivot
+			if (arr[j] <= pivot) {
+				i++;
+
+				// swap arr[i] and arr[j]
+				double temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		double temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+
+		return i + 1;
+	}
+
+	public void quickSortDouble(double arr[], int l, int h) {
+		
+		int[] stack = new int[h - l + 1];
+
+		
+		int top = -1;
+
+		
+		stack[++top] = l;
+		stack[++top] = h;
+
+		
+		while (top >= 0) {
+			
+			h = stack[top--];
+			l = stack[top--];
+
+			
+			int p = partitionDouble(arr, l, h);
+
+			
+			if (p - 1 > l) {
+				stack[++top] = l;
+				stack[++top] = p - 1;
+			}
+
+			
+			if (p + 1 < h) {
+				stack[++top] = p + 1;
+				stack[++top] = h;
+			}
+		}
+	}
+
 	public void mergeSort(int arr[], int n)
     {
          
@@ -179,6 +239,89 @@ public class LogicSort {
             k++;
         }
     }
+    
+    public void mergeSortDouble(double arr[], int n)
+    {
+         
+        
+        int curr_size; 
+                     
+        
+        int left_start;
+                         
+        
+        for (curr_size = 1; curr_size <= n-1; 
+                      curr_size = 2*curr_size)
+        {
+             
+           
+            for (left_start = 0; left_start < n-1;
+                        left_start += 2*curr_size)
+            {
+                
+                int mid = left_start + curr_size - 1;
+         
+                int right_end = Math.min(left_start 
+                             + 2*curr_size - 1, n-1);
+         
+                
+                mergeDouble(arr, left_start, mid, right_end);
+            }
+        }
+    }
+     
+    
+    static void mergeDouble(double arr[], int l, int m, int r)
+    {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+     
+        
+        double L[] = new double[n1];
+        double R[] = new double[n2];
+     
+        
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1+ j];
+     
+        
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+     
+        
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+     
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
 
 	public static void main(String[] args) {
 
