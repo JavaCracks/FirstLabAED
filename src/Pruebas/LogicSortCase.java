3d49a25d9febf3f2a4176e34554c522b35a0bce8
danie <danie@172.30.192.150>
2018-08-23 21:53:21 -0500

    Create testCase of the main class of the model

---
+++ src/Pruebas/LogicSortCase.java
@@ -0,0 +1,33 @@
+package Pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import model.LogicSort;
+
+public class LogicSortCase extends TestCase{
+	
+		
+		private LogicSort logic;
+		int[] array= {22,13,6,96,46,24};
+		
+		public void stageOne() {
+			logic=new LogicSort();
+		}
+		
+		@Test
+		public void testCountingSort(int [] arreglo) {
+			
+			stageOne();
+			
+			
+			
+			
+		} 
+
+		
+	}
+
+
