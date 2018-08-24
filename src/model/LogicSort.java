6c360f4f4bd4908343b06aca7db222c77fdddf5d
danie <danie@172.30.192.150>
2018-08-23 20:00:23 -0500

    Remove the method of Tim Sort and implement the method of Merge Sort

--- src/model/LogicSort.java
+++ src/model/LogicSort.java
@@ -65,49 +65,129 @@ public class LogicSort {
 	}
 
 	public void quickSort(int arr[], int l, int h) {
-		// Create an auxiliary stack
+		
 		int[] stack = new int[h - l + 1];
 
-		// initialize top of stack
+		
 		int top = -1;
 
-		// push initial values of l and h to stack
+		
 		stack[++top] = l;
 		stack[++top] = h;
 
-		// Keep popping from stack while is not empty
+		
 		while (top >= 0) {
-			// Pop h and l
+			
 			h = stack[top--];
 			l = stack[top--];
 
-			// Set pivot element at its correct position
-			// in sorted array
+			
 			int p = partition(arr, l, h);
 
-			// If there are elements on left side of pivot,
-			// then push left side to stack
+			
 			if (p - 1 > l) {
 				stack[++top] = l;
 				stack[++top] = p - 1;
 			}
 
-			// If there are elements on right side of pivot,
-			// then push right side to stack
+			
 			if (p + 1 < h) {
 				stack[++top] = p + 1;
 				stack[++top] = h;
 			}
 		}
 	}
+	
+	public void mergeSort(int arr[], int n)
+    {
+         
+        
+        int curr_size; 
+                     
+        
+        int left_start;
+                         
+        
+        for (curr_size = 1; curr_size <= n-1; 
+                      curr_size = 2*curr_size)
+        {
+             
+           
+            for (left_start = 0; left_start < n-1;
+                        left_start += 2*curr_size)
+            {
+                
+                int mid = left_start + curr_size - 1;
+         
+                int right_end = Math.min(left_start 
+                             + 2*curr_size - 1, n-1);
+         
+                
+                merge(arr, left_start, mid, right_end);
+            }
+        }
+    }
+     
+    
+    static void merge(int arr[], int l, int m, int r)
+    {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+     
+        
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+     
+        
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1+ j];
+     
+        
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+     
+        
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+     
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
 	public static void main(String[] args) {
 
 		int arreglo[] = { 12, 23, 15, 4, 7, 8, 1, 2, 3 };
 
 		LogicSort l = new LogicSort();
+		l.mergeSort(arreglo, arreglo.length);
 
-		System.out.println(Arrays.toString(l.countingSort(arreglo)));
+		System.out.println(Arrays.toString(arreglo));
 
 	}
 
